464a19e85aaaa68bda077238fc02c729a227eb7b
heopill <heopill@naver.com>
2025-03-20 23:45:58 +0900

    feat: make Lv5 code
    Lv5 기능 구현

--- BaseballGame/RecordManager.swift
+++ BaseballGame/RecordManager.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class RecordManager {
+    var trialCount: [Int] = []
+    
+    func add (_ count:Int){
+        trialCount.append(count)
+    }
+    
+    func showRecords() {
+        print("\n< 게임 기록 보기 >")
+        for i in 0...trialCount.count-1 {
+            print("\(i+1)번째 게임 : 시도 횟수 - \(trialCount[i])")
+        }
+        print("")
+    }
+}
